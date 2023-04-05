dd4375b74904de27fb052d168557eb51fb5326c7
Zainab <zainabjafari998@gmail.com>
2023-04-05 16:56:48 +0200

    G nivå klar

--- Model/productModel.js
+++ Model/productModel.js
@@ -12,7 +12,7 @@ exports.createNewProduct = (req, res) =>{
     }
 
 
-    Product.create({name, description, price, imageURL})
+    Product.create({name, description, price,  })
     .then(data => {
         res.status(201).json(data)
     })
@@ -37,3 +37,61 @@ exports.getAllProduct = (req, res) =>{
     })
 }
 
+exports.getProductById = async (req, res) =>{
+
+    const product = await Product.findById(req.params.id)
+    
+    if(!product){
+        return res.status(404).json({message: 'Could not find the product'})
+    }
+
+    res.status(200).json(product)
+    
+}
+
+exports.uppdateProduct = async (req, res) =>{
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    
+    if(!product){
+      return res.status(404).json({message: 'Could not find the product'})
+    }
+
+    res.status(200).json(product)
+
+}
+
+
+
+
+exports.deleteProduct = (req, res) =>{
+
+    Product.findByIdAndDelete(req.params.id)
+    .then(product => {
+        if(!product){
+           return res.status(404).json({message: 'Could not find the product'})
+        }
+        res.status(200).json({id: product._id})
+    })
+    .catch(err => {
+        res.status(500).json({message: 'Something went wrong when deleting the product' ,
+        err: err.message})
+        
+    })
+}
+
+
+
+
+
+
+// exports.deleteProcuct = (req, res)=> {
+
+//    Product.findByIdAndDelete(req.params.id)
+//      .then(product =>{
+//       if(!product){
+//         return res.status(404).json({message: 'Could not find the product'})
+//     }
+
+//    res.status(200).json({id: product._id})
+
+// })
--- controllers/productControl.js
+++ controllers/productControl.js
@@ -4,6 +4,9 @@ const productModel = require('../Model/productModel')
 
 router.post('/', productModel.createNewProduct)
 router.get('/', productModel.getAllProduct)
+router.get('/:id', productModel.getProductById)
+router.put('/:id', productModel.uppdateProduct)
+router.delete('/:id', productModel.deleteProduct)
 
 
 module.exports = router;
